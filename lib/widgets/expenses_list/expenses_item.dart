ae40e155d735e324f8d74c34632559f3bbae08d3
Haldiant <nhalwidinato@gmail.com>
2025-09-08 21:20:06 +0700

    Add date formatting and category icons to Expense model and ExpensesItem widget

--- lib/widgets/expenses_list/expenses_item.dart
+++ lib/widgets/expenses_list/expenses_item.dart
@@ -26,9 +26,9 @@ class ExpensesItem extends StatelessWidget {
                 Spacer(),
                 Row(
                   children: [
-                    Icon(Icons.alarm),
+                    Icon(categoryIcons[expense.category]),
                     SizedBox(width: 8.0),
-                    Text(expense.date.toString()),
+                    Text(expense.formattedDate),
                   ],
                 ),
               ],
