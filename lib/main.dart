d83178a43ae05ae157e5469302ae23ef96130601
Haldiant <nhalwidinato@gmail.com>
2025-11-11 22:23:00 +0700

    Refactor theme setup in main.dart to improve color scheme configuration

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,20 @@
 import 'package:expense_tracker/widgets/expenses.dart';
 import 'package:flutter/material.dart';
 
+var kColorScheme = ColorScheme.fromSeed(
+  seedColor: Color.fromARGB(255, 69, 100, 200),
+);
+
 void main() {
   runApp(
     MaterialApp(
+      theme: ThemeData().copyWith(
+        colorScheme: kColorScheme,
+        appBarTheme: AppBarTheme().copyWith(
+          backgroundColor: kColorScheme.onPrimaryContainer,
+          foregroundColor: kColorScheme.primaryContainer,
+        ),
+      ),
       home: Expenses(),
     ),
   );
